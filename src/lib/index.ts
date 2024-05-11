44e83421273f58d445eb8820fdac8333e982849b
Paul Gathondu <aulgondu@gmail.com>
2024-05-10 20:00:57 -0700

    feat: add landing route

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
 export { default as dynamicRouter } from "./next.dynamic.mjs";
 export { default as constructMetadata } from "./seo";
 export { default as cn } from "./cn";
-export * from "./font";
+export * from "./fonts";
