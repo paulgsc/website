f2c32a197f64ae88fce01a0d3595ed339cf85a70
Paul Gathondu <aulgondu@gmail.com>
2024-05-11 20:50:23 -0700

    feat: open-hours

--- src/components/open-hours.tsx
+++ src/components/open-hours.tsx
@@ -9,6 +9,7 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "./ui/drawer";
+import { Separator } from "./ui/separator";
 
 export default function OpenHours() {
   return (
@@ -32,12 +33,35 @@ export default function OpenHours() {
         </svg>
       </DrawerTrigger>
       <DrawerContent className=" container flex items-center justify-center bg-gradient-to-br from-cyan-400 to-teal-950">
+        <Separator className="my-4 2xl:my-8 opacity-0" />
         <DrawerHeader>
-          <DrawerTitle>
-            <h1 className="uppercase">open</h1>
-            <h2 className="uppercase">hours</h2>
+          <DrawerTitle className="flex flex-col items-center ">
+            <h1 className="xl:text-5xl 2xl:text-6xl tracking-wide uppercase font-bold text-white">
+              open
+            </h1>
+            <div className="relative h-12 xl:w-36 2xl:w-40 flex items-center justify-center text-wrap before:absolute before:left-0 before:w-2.5 before:top before:h-2.5 before:bg-white before:rounded-full after:absolute after:right-0 after:w-2.5 after:h-2.5 after:bg-white after:rounded-full">
+              <h2 className="xl:text-3xl 2xl:text-4xl tracking-wide text-center uppercase font-bold text-white ">
+                hours
+              </h2>
+            </div>
           </DrawerTitle>
-          <DrawerDescription>This action cannot be undone.</DrawerDescription>
+          <DrawerDescription className=" mx-auto p-2.5 px-4 border border-white mt-6">
+            <article className="flex flex-col gap-y-2.5">
+              <h3 className="grid grid-cols-3 text-center items-center text-2xl uppercase font-bold text-white">
+                <span>monday</span>{" "}
+                <span className="inline-flex justify-center items-end">
+                  <span className="text-xs font-normal tracking-tighter relative after:absolute after:inset-0 after:h-0.5 after:bg-white before:absolute before:inset-x-0 before:bottom-0 before:h-0.5 before:bg-white">
+                    thru
+                  </span>
+                </span>
+                <span>friday</span>
+              </h3>
+              <p className="grid grid-cols-3 items-center text-center text-xl xl:text-4xl 2xl:text-5xl tracking-tight uppercase font-bold text-white">
+                <span className="">12:00 pm</span> <span>-</span>{" "}
+                <span>6:00 pm</span>
+              </p>
+            </article>
+          </DrawerDescription>
         </DrawerHeader>
         <DrawerFooter>
           <DrawerClose>
