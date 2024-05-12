f3d93b8ff9881921d4fe59ceb96e21e8377427da
Paul Gathondu <aulgondu@gmail.com>
2024-05-11 23:19:17 -0700

    fix: make about card more responsive

--- src/app/(landing)/components/about.tsx
+++ src/app/(landing)/components/about.tsx
@@ -12,14 +12,10 @@ import { Pilot } from "@/assets";
 
 export const About = () => {
   return (
-    <section id="about" className="container py-24 sm:py-32">
+    <section className=" max-sm:w-full md:container ">
       <div className="bg-muted/50 border rounded-lg py-12">
         <div className="px-6 flex flex-col-reverse md:flex-row gap-8 md:gap-12">
-          <Image
-            src={Pilot}
-            alt=""
-            className="w-[300px] object-contain rounded-lg"
-          />
+          <Image src={Pilot} alt="" className="object-contain rounded-lg" />
           <div className="bg-green-0 flex flex-col justify-between">
             <div className="pb-6">
               <h2 className="text-3xl md:text-4xl font-bold">
@@ -28,7 +24,7 @@ export const About = () => {
                 </span>
                 Company
               </h2>
-              <p className="text-xl text-muted-foreground mt-4">
+              <p className="text-base lg:text-xl text-wrap text-muted-foreground mt-4">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -3,8 +3,8 @@ import { About } from "./components/about";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+      <div className="z-10 w-full lg:max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <About />
       </div>
     </main>
