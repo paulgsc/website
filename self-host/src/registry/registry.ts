93baa8ace9abfdb75d487fddf5728777f6820db7
Paul Gathondu <aulgondu@gmail.com>
2024-05-16 16:32:31 -0700

    feat: cp more files for mdx

--- self-host/src/registry/registry.ts
+++ self-host/src/registry/registry.ts
@@ -1,3 +1,6 @@
-import { Registry } from "@/registry/schema";
+import { blocks } from "@/registry/blocks"
+import { examples } from "@/registry/examples"
+import { Registry } from "@/registry/schema"
+import { ui } from "@/registry/ui"
 
-export const registry: Registry = [];
+export const registry: Registry = [...ui, ...examples, ...blocks]
