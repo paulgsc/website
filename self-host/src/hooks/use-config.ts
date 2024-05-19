2cbb8bf1a148aff234a9177f37a8ef324de0ba65
Paul Gathondu <aulgondu@gmail.com>
2024-05-19 11:51:20 -0700

    fix: update congig style to default

--- self-host/src/hooks/use-config.ts
+++ self-host/src/hooks/use-config.ts
@@ -1,21 +1,21 @@
-import { Style, Theme } from "@/types";
-import { create } from "zustand";
+import { Style, Theme } from "@/types"
+import { create } from "zustand"
 
 type Config = {
-  style: Style["name"];
-  theme: Theme["name"];
-  radius: number;
-};
+  style: Style["name"]
+  theme: Theme["name"]
+  radius: number
+}
 
 const initialConfig: Config = {
-  style: "new-york",
+  style: "default",
   theme: "zinc",
   radius: 0.5,
-};
+}
 
 export const useConfig = create<Config>((set) => ({
   ...initialConfig,
   setStyle: (style: Style["name"]) => set((state) => ({ ...state, style })),
   setTheme: (theme: Theme["name"]) => set((state) => ({ ...state, theme })),
   setRadius: (radius: number) => set((state) => ({ ...state, radius })),
-}));
+}))
