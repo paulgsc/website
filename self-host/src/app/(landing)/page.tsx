abfd4389f4e45ccf0a353a62241128d697f2eab9
Paul Gathondu <aulgondu@gmail.com>
2024-05-20 18:56:24 -0700

    feat: add youtube badge link

--- self-host/src/app/(landing)/page.tsx
+++ self-host/src/app/(landing)/page.tsx
@@ -1,11 +1,14 @@
 import type { FC } from "react"
 
+import YoutubeBadge from "@/components/youtube-badge"
+
 import { About } from "./components/about"
 
 const Home: FC = () => {
   return (
     <main className="flex  flex-col items-center justify-between ">
       <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex lg:max-w-5xl">
+        <YoutubeBadge />
         <About />
       </div>
     </main>
