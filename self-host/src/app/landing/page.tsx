765eb1383a29cdac7f558dbee2b961c1e93ba467
Paul Gathondu <aulgondu@gmail.com>
2024-05-24 15:42:11 -0700

    test: add schadcn autoplay demo

--- self-host/src/app/landing/page.tsx
+++ self-host/src/app/landing/page.tsx
@@ -1,16 +1,42 @@
-import type { FC } from "react"
+import { useRef, type FC } from "react"
 
-import YoutubeBadge from "@/components/youtube-badge"
-
-import { About } from "./components/about"
+import Autoplay from "@/lib/embla-fork/autoplay"
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
 const Home: FC = () => {
+  const plugin = useRef(Autoplay({ delay: 2000, stopOnInteraction: true }))
+
   return (
     <main className="flex  flex-col items-center justify-between ">
-      <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex lg:max-w-5xl">
-        <YoutubeBadge />
-        <About />
-      </div>
+      <Carousel
+        plugins={[plugin.current]}
+        className="w-full max-w-xs"
+        onMouseEnter={plugin.current.stop}
+        onMouseLeave={plugin.current.play}
+      >
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </main>
   )
 }
