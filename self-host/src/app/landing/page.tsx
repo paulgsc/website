ec9654e4d15083433967b2771c47b203b53a601f
Paul Gathondu <aulgondu@gmail.com>
2024-05-24 16:00:14 -0700

    feat: carousel indicators init

--- self-host/src/app/landing/page.tsx
+++ self-host/src/app/landing/page.tsx
@@ -3,6 +3,7 @@
 import { useRef, type FC } from "react"
 
 import Autoplay from "@/lib/embla-fork/autoplay"
+import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import {
   Carousel,
@@ -36,6 +37,17 @@ const Home: FC = () => {
             </CarouselItem>
           ))}
         </CarouselContent>
+        <div className="absolute bottom-5 left-1/2 z-30 flex -translate-x-1/2 space-x-3 rtl:space-x-reverse">
+          {Array.from({ length: 4 }).map((_, index) => (
+            <Button
+              key={index}
+              className="m-0 size-3 rounded-full p-0"
+              aria-current="true"
+              aria-label="Slide 1"
+              data-carousel-slide-to="0"
+            ></Button>
+          ))}
+        </div>
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
