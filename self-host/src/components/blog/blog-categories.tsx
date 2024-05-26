384b53c2caabe5227da074d97523b62e1343a557
Paul Gathondu <aulgondu@gmail.com>
2024-05-26 10:19:55 -0700

    refactor: create blog cont array

--- self-host/src/components/blog/blog-categories.tsx
+++ self-host/src/components/blog/blog-categories.tsx
@@ -4,12 +4,21 @@ import { badgeVariants } from "@/components/ui/badge"
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
 import ActiveLink from "@/components/active-link"
 
+const categories = [
+  "all posts",
+  "updates",
+  "odoo",
+  "next js",
+  "self hosting",
+  "rust",
+  "projects",
+] as const
 const BlogCategories = () => {
   return (
     <nav className="mt-1.5 flex w-full justify-center">
       <ScrollArea className=" max-w-xs grow whitespace-nowrap py-3 md:max-w-2xl xl:max-w-[980px] 2xl:max-w-[1460px]">
         <div className="flex w-full flex-1 items-center justify-around gap-6">
-          {Array.from({ length: 5 }, (_, index) => (
+          {categories.map((category, index) => (
             <ActiveLink
               key={index}
               href={"/"}
@@ -17,11 +26,11 @@ const BlogCategories = () => {
               rel="noreferrer"
               className={cn(
                 badgeVariants({ variant: "accent" }),
-                "hover:bg-background rounded-md pe-2.5 ps-2.5 saturate-150"
+                "hover:bg-background shrink-0 rounded-md pe-2.5 ps-2.5 text-sm capitalize tracking-tight saturate-150"
               )}
               activeClassName={cn(badgeVariants({ variant: "default" }))}
             >
-              API Reference
+              {category}
             </ActiveLink>
           ))}
         </div>
