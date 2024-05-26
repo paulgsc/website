9aa9fc946f977736142ed1aead9784336bf5a92c
Paul Gathondu <aulgondu@gmail.com>
2024-05-26 11:29:16 -0700

    refactor: css syles, remove unused contenlayer calls for now

--- self-host/src/app/(markdown)/blog/[[...slug]]/page.tsx
+++ self-host/src/app/(markdown)/blog/[[...slug]]/page.tsx
@@ -1,14 +1,5 @@
-// eslint-disable-next-line import/order
-// eslint-disable-next-line import/order
-import { notFound } from "next/navigation"
-
-import "@/styles/mdx.css"
-
 import Image from "next/image"
-import { generateStaticParams, getPageFromParams } from "@/lib"
-import type { ContentlayerPagePropsWithoutRootPath } from "@/types"
 
-import { getTableOfContents } from "@/lib/toc"
 import {
   Card,
   CardContent,
@@ -19,23 +10,9 @@ import {
 // eslint-disable-next-line import/order
 
 import BlogCategories from "@/components/blog/blog-categories"
+import RecommededBlogs from "@/components/blog/recomended-blogs"
 
-// We add the rootPath to the params since the [[...slug]] pattern in Next.js
-// is exclusive of the path and only generates the params for the slug part.
-const rootPath = ["blog"]
-
-generateStaticParams({ rootPath: rootPath })
-
-const Page = async ({ params }: ContentlayerPagePropsWithoutRootPath) => {
-  const doc = await getPageFromParams({ params, rootPath })
-
-  if (!doc) {
-    notFound()
-  }
-
-  const toc = await getTableOfContents(doc.body.raw)
-  console.log(toc)
-
+const Page = () => {
   return (
     <div className="flex flex-1 flex-col">
       <section className="max-w-md:flex max-w-md:shrink max-w-md:flex-col w-full  md:grid md:grid-cols-[.1fr_.35fr_.45fr_.1fr] 2xl:grid-cols-[.1fr_.35fr_.45fr_.1fr]">
@@ -58,7 +35,7 @@ const Page = async ({ params }: ContentlayerPagePropsWithoutRootPath) => {
         </Card>
       </section>
       <BlogCategories />
-      {/* <RecommededBlogs blogAuthor="Paul M Gathondu" /> */}
+      <RecommededBlogs blogAuthor="Paul M Gathondu" />
     </div>
   )
 }
--- self-host/src/components/blog/recomended-blogs.tsx
+++ self-host/src/components/blog/recomended-blogs.tsx
@@ -3,24 +3,15 @@ import Link from "next/link"
 
 import { listenNowAlbums } from "@/config/data/albums"
 import { mapAuthorToCardAuthors } from "@/lib/utils/blog-utils"
-import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
-  CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import {
-  Pagination,
-  PaginationContent,
-  PaginationItem,
-} from "@/components/ui/pagination"
 import { Separator } from "@/components/ui/separator"
 import AvatarGroup from "@/components/avatar-goup"
 import FormattedTime from "@/components/formatted-time"
-import { Icons } from "@/components/icons"
 
 import BlogImage from "./blog-image-card"
 
@@ -36,15 +27,8 @@ const RecommededBlogs: FC<RecommendedBlotProps> = ({
   const authors = blogAuthor ? mapAuthorToCardAuthors(blogAuthor) : []
   const avatars = authors.map(({ fullName, src }) => ({ alt: fullName, src }))
   return (
-    <Card className=" border-none shadow-none">
-      <CardHeader className="flex w-fit flex-row items-start">
-        <div className="grid gap-0.5">
-          <CardTitle className="group flex items-center gap-2 text-lg">
-            Recommended Articles
-          </CardTitle>
-        </div>
-      </CardHeader>
-      <CardContent className="bg-muted/40 rounded-sm p-6 text-sm">
+    <Card className="mt-4 hidden w-full border-none shadow-none md:block">
+      <CardContent className="bg-muted/40 flex grow-0 items-center justify-evenly rounded-sm">
         <div className="flex space-x-4 pb-4">
           {listenNowAlbums.map((album) => (
             <article key={album.name} className="block w-full max-w-sm">
@@ -91,24 +75,6 @@ const RecommededBlogs: FC<RecommendedBlotProps> = ({
           ))}
         </div>
       </CardContent>
-      <CardFooter className="mb-16 hidden flex-row items-center  p-1.5 md:flex ">
-        <Pagination className="ml-auto mr-0 w-auto">
-          <PaginationContent className="space-x-2.5">
-            <PaginationItem>
-              <Button size="icon" variant="outline" className="size-6">
-                <Icons.chevronRight className="size-3.5 rotate-180" />
-                <span className="sr-only">Previous</span>
-              </Button>
-            </PaginationItem>
-            <PaginationItem>
-              <Button size="icon" variant="outline" className="size-6">
-                <Icons.chevronRight className="size-3.5" />
-                <span className="sr-only">Next</span>
-              </Button>
-            </PaginationItem>
-          </PaginationContent>
-        </Pagination>
-      </CardFooter>
     </Card>
   )
 }
