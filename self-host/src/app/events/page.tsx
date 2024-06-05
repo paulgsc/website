c1f2d18a2246e643923f9de91a970904a802760a
Paul Gathondu <aulgondu@gmail.com>
2024-06-04 18:16:37 -0700

    feat: add events route

--- self-host/src/app/events/page.tsx
+++ self-host/src/app/events/page.tsx
@@ -1,7 +1,12 @@
+<<<<<<< HEAD
 import EventsTimelineWrapper from "@/components/my-events"
 
 const Page = () => {
   return <EventsTimelineWrapper />
+=======
+const Page = () => {
+  return <></>
+>>>>>>> ca472f6 (feat: add events route)
 }
 
 export default Page
