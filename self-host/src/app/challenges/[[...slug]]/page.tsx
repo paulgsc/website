972aff85e14d7374346d4ecb0c2f976c95a346b4
Paul Gathondu <aulgondu@gmail.com>
2024-06-16 13:39:24 -0700

    feat: challenge bg styles

--- self-host/src/app/challenges/[[...slug]]/page.tsx
+++ self-host/src/app/challenges/[[...slug]]/page.tsx
@@ -3,9 +3,13 @@ import { cn } from "@/lib"
 const ChallengesHome = () => {
   return (
     <div
+      style={{
+        backgroundRepeat: "no-repeat, repeat",
+      }}
       className={cn(
         "bg-countdown-timer absolute inset-0",
-        "bg-[url('./assets/challenges/countdown-timer/hills.svg')] bg-bottom bg-no-repeat"
+        "bg-[url('./assets/challenges/countdown-timer/hills.svg'),_url('./assets/challenges/countdown-timer/stars.svg')]",
+        "bg-contain bg-[position:bottom,_top]"
       )}
     ></div>
   )
