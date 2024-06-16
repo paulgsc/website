3364ee4fa9808be0f7d43ef3802187c89fe8ea9d
Paul Gathondu <aulgondu@gmail.com>
2024-06-16 14:55:05 -0700

    feat: add countdown compouond card

--- self-host/src/components/countdown-timer/bg-card/index.tsx
+++ self-host/src/components/countdown-timer/bg-card/index.tsx
@@ -1,24 +1,42 @@
-import type { FC, HTMLAttributes } from "react"
+import type { HTMLAttributes } from "react"
+import { forwardRef } from "react"
 import { cn } from "@/lib"
 
-const CountDownCard: FC<HTMLAttributes<HTMLDivElement>> = ({
-  className,
-  ...props
-}) => {
-  return (
-    <div
-      style={{
-        backgroundRepeat: "no-repeat, repeat",
-      }}
-      className={cn(
-        "bg-countdown-timer absolute inset-0",
-        "bg-[url('./assets/challenges/countdown-timer/hills.svg'),_url('./assets/challenges/countdown-timer/stars.svg')]",
-        "bg-contain bg-[position:bottom,_top]",
-        className
-      )}
-      {...props}
-    />
-  )
-}
+const CountDownCard = forwardRef<
+  HTMLDivElement,
+  HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    style={{
+      backgroundRepeat: "no-repeat, repeat",
+    }}
+    className={cn(
+      "bg-countdown-timer absolute inset-0",
+      "bg-[url('./assets/challenges/countdown-timer/hills.svg'),_url('./assets/challenges/countdown-timer/stars.svg')]",
+      "bg-contain bg-[position:bottom,_top]",
+      className
+    )}
+    {...props}
+  />
+))
 
-export default CountDownCard
+CountDownCard.displayName = "CountDownCard"
+
+const CountDowndTitle = forwardRef<
+  HTMLHeadingElement,
+  HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+  <h1
+    ref={ref}
+    {...props}
+    className={cn(
+      "text-center text-xl uppercase leading-10 tracking-[0.5rem] text-white",
+      className
+    )}
+  />
+))
+
+CountDowndTitle.displayName = "Title"
+
+export { CountDownCard, CountDowndTitle }
--- self-host/src/components/countdown-timer/index.tsx
+++ self-host/src/components/countdown-timer/index.tsx
@@ -1,7 +1,13 @@
-import CountDownCard from "./bg-card"
+import { CountDownCard, CountDowndTitle } from "./bg-card"
 
 const Countdown = () => {
-  return <CountDownCard></CountDownCard>
+  return (
+    <CountDownCard className="flex flex-col items-center justify-center">
+      <CountDowndTitle>
+        we<span className="-ms-1 italic">&apos;</span>re launching soon
+      </CountDowndTitle>
+    </CountDownCard>
+  )
 }
 
 export default Countdown
