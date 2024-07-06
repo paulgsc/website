0b23a19de649e734e66d64d613d27bd3c8cec4b3
Paul Gathondu <aulgondu@gmail.com>
2024-07-05 23:48:00 -0700

    feat: add drizzle config

---
+++ self-host/drizzle.config.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv"
+import type { Config } from "drizzle-kit"
+
+dotenv.config()
+
+// We need to make sure the in the tsconfig.json file, we need to change the target at least to 'ES6'
+export default {
+  schema: "./db/schema/index.ts",
+  out: "./drizzle",
+  dialect: "sqlite",
+  driver: "turso",
+  dbCredentials: {
+    url: process.env.DB_URL!,
+  },
+} satisfies Config
