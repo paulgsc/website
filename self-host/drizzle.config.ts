e279a49598c188a42fba0c0d0dc8cb3206871db2
Paul Gathondu <aulgondu@gmail.com>
2024-07-06 00:04:57 -0700

    fix: wrong path name for drizzle config

--- self-host/drizzle.config.ts
+++ self-host/drizzle.config.ts
@@ -5,7 +5,7 @@ dotenv.config()
 
 // We need to make sure the in the tsconfig.json file, we need to change the target at least to 'ES6'
 export default {
-  schema: "./db/schema.ts",
+  schema: "./src/db/schema.ts",
   out: "./drizzle",
   dialect: "sqlite",
   driver: "turso",
