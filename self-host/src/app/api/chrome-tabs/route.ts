2d52928a7e79627b57859b7b7c8663d7f616afc4
Paul Gathondu <aulgondu@gmail.com>
2024-07-06 18:39:40 -0700

    feat: link api to orm

--- self-host/src/app/api/chrome-tabs/route.ts
+++ self-host/src/app/api/chrome-tabs/route.ts
@@ -1,21 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from "next"
+import { revalidatePath } from "next/cache"
+import { db } from "@/db"
+import type { BrowserTab } from "@/db/schema"
+import { browserTabs } from "@/db/schema"
 
+import type { Tab } from "@/types/api/chrome"
+import { TabSchema } from "@/types/api/chrome"
 import { KnownError } from "@/lib/errors"
 
 export async function POST(req: NextApiRequest, res: NextApiResponse) {
   try {
-    // const { title, description } = await req.body()
+    const reqBody = await req.body
+    const parsedReqBody = TabSchema.safeParse(reqBody)
 
-    // db.insert(notes)
-    //   .values({
-    //     description: description,
-    //     id: crypto.randomUUID(),
-    //     title: title,
-    //   })
-    //   .run()
-    // // to apply the changes without reload the page
-    // revalidatePath("/")
-    console.log(req)
+    if (!parsedReqBody.success)
+      throw new KnownError(parsedReqBody.error.message)
+
+    const chromeTabs: Tab = parsedReqBody.data satisfies BrowserTab
+
+    db.insert(browserTabs)
+      .values({ ...chromeTabs })
+      .run()
+
+    // to apply the changes without reload the page
+    revalidatePath("/")
   } catch (error) {
     if (error instanceof KnownError)
       return res.status(500).json({ error: error.message })
--- self-host/src/db/schema.ts
+++ self-host/src/db/schema.ts
@@ -1,9 +1,28 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core"
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core"
 
-export const notes = sqliteTable("notes", {
-  id: text("id").primaryKey(),
-  title: text("title").notNull(),
-  description: text("description").notNull(),
+export const browserTabs = sqliteTable("browser_tabs", {
+  id: integer("id").primaryKey(),
+  status: text("status", { enum: ["loading", "complete"] }),
+  index: integer("index").notNull(),
+  openerTabId: integer("opener_tab_id"),
+  title: text("title"),
+  url: text("url"),
+  pendingUrl: text("pending_url"),
+  pinned: integer("pinned", { mode: "boolean" }).notNull(),
+  highlighted: integer("highlighted", { mode: "boolean" }).notNull(),
+  windowId: integer("window_id").notNull(),
+  active: integer("active", { mode: "boolean" }).notNull(),
+  favIconUrl: text("fav_icon_url"),
+  incognito: integer("incognito", { mode: "boolean" }).notNull(),
+  selected: integer("selected", { mode: "boolean" }).notNull(),
+  audible: integer("audible", { mode: "boolean" }),
+  discarded: integer("discarded", { mode: "boolean" }).notNull(),
+  autoDiscardable: integer("auto_discardable", { mode: "boolean" }).notNull(),
+  width: integer("width"),
+  height: integer("height"),
+  sessionId: text("session_id"),
+  groupId: integer("group_id").notNull(),
+  lastAccessed: integer("last_accessed"),
 })
 
-export type Note = typeof notes.$inferSelect
+export type BrowserTab = typeof browserTabs.$inferSelect
