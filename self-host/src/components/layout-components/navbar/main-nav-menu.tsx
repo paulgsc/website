1fdeb529a193f8193d39a689863f9ffe29532026
Paul Gathondu <aulgondu@gmail.com>
2024-07-15 02:16:19 -0700

    refactor: update nav menu

--- self-host/src/components/layout-components/navbar/main-nav-menu.tsx
+++ self-host/src/components/layout-components/navbar/main-nav-menu.tsx
@@ -71,7 +71,7 @@ const MainNavigationMenu = () => {
           </NavigationMenuContent>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Freelancing</NavigationMenuTrigger>
+          <NavigationMenuTrigger>Streaming</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
               {navMenuData.map((component) => (
