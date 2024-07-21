aad8f5a0fbdc3b42f8f687e428318285ff6d1c91
Paul Gathondu <aulgondu@gmail.com>
2024-07-21 00:35:32 -0700

    feat: add my-superpowers layout page

---
+++ self-host/src/components/layout-components/components/my-superpowers-layout.tsx
@@ -0,0 +1,9 @@
+import { type FC } from "react"
+
+import RadarChartPlayground from "@/components/charts/radar"
+
+const MySuperPowersLayout: FC = () => {
+  return <RadarChartPlayground />
+}
+
+export default MySuperPowersLayout
