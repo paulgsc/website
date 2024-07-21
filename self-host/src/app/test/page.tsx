0a2cb4eb039cb25a8a9c9eaa7ca5907408ba2a6b
Paul Gathondu <aulgondu@gmail.com>
2024-07-21 00:36:06 -0700

    chore: delete test page route

--- self-host/src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC } from "react"
-
-import RadarChartPlayground from "@/components/charts/radar"
-
-const Page: FC = () => {
-  return <RadarChartPlayground />
-}
-
-export default Page
