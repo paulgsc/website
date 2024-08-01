0c0eff0dc9b42f9db5b2805424d460d0ab079fb5
Paul Gathondu <aulgondu@gmail.com>
2024-07-31 17:15:12 -0700

    feat: create type inference for role permissions

--- self-host/src/types/auth/roles.ts
+++ self-host/src/types/auth/roles.ts
@@ -1,48 +1,66 @@
 import { z } from "zod"
 
-// Define the roles and access levels
-const APP_USER_ROLES = [
-  "subscriber",
-  "github",
-  "unknown",
-  "recruiter",
-  "superuser",
-] as const
+type APP_USER_ROLES =
+  | "subscriber"
+  | "github"
+  | "unknown"
+  | "recruiter"
+  | "superuser"
+type APP_USER_ACCESS =
+  | "portfolio"
+  | "blog"
+  | "fundme"
+  | "trial"
+  | "personal"
+  | "events"
 
-const APP_USER_ACCESS = [
-  "portfolio",
+const createEnumSchema = <T extends string>(values: Array<T>) =>
+  z.enum(values as [T, ...Array<T>])
+
+export const roleSchema = createEnumSchema<APP_USER_ROLES>([
+  "github",
+  "recruiter",
+  "subscriber",
+  "superuser",
+  "unknown",
+])
+export const accessSchema = createEnumSchema<APP_USER_ACCESS>([
   "blog",
   "events",
   "fundme",
-  "trial",
   "personal",
-] as const
-
-export const roleSchema = z.enum(APP_USER_ROLES)
-export const accessSchema = z.enum(APP_USER_ACCESS)
+  "portfolio",
+  "trial",
+])
 
 export type AppUserRole = z.infer<typeof roleSchema>
 export type AppUserAccess = z.infer<typeof accessSchema>
 
-export const allowedRoleAccess: Record<
-  typeof roleSchema._type,
-  ReadonlyArray<typeof accessSchema._type>
-> = {
+type PossibleRoleAccess<T extends string, P extends string> = {
+  // eslint-disable-next-line no-unused-vars
+  [K in T]: Array<P>
+}
+
+export const allowedRoleAccess = {
   subscriber: ["portfolio", "blog", "events"],
   github: ["portfolio", "blog"],
   unknown: ["trial"],
   recruiter: ["portfolio"],
   superuser: ["blog", "events", "fundme", "personal", "portfolio", "trial"],
-} as const satisfies Record<AppUserRole, ReadonlyArray<AppUserAccess>>
+} satisfies PossibleRoleAccess<APP_USER_ROLES, APP_USER_ACCESS>
 
 export const roleAccessPairSchema = z
   .object(
     Object.fromEntries(
       Object.entries(allowedRoleAccess).map(([role, accesses]) => [
-        role as typeof roleSchema._type, // Explicitly type the role as roleSchema._type
+        role as APP_USER_ROLES, // Explicitly type the role as roleSchema._type
         z
           .array(accessSchema)
-          .refine((arr) => arr.every((val) => accesses.includes(val))),
+          .refine((arr) =>
+            arr.every((val) =>
+              (accesses as Array<APP_USER_ACCESS>).includes(val)
+            )
+          ),
       ])
     ) as unknown as Record<
       typeof roleSchema._type,
@@ -52,6 +70,18 @@ export const roleAccessPairSchema = z
   .strict()
 
 export type RoleAccessPairs = typeof allowedRoleAccess
+export type RolesWithAccess<R extends APP_USER_ROLES> =
+  (typeof allowedRoleAccess)[R][number]
+
+export type RoleWithAllAccess<Access extends Array<APP_USER_ACCESS>> = {
+  [Role in keyof RoleAccessPairs]: Access[number] extends RoleAccessPairs[Role][number]
+    ? Role
+    : never
+}[keyof RoleAccessPairs]
+
+export type RequiredAccess<T extends APP_USER_ACCESS> = T
+
+export type AllRequiredAccess<T extends APP_USER_ACCESS> = [T, ...Array<T>]
 
 export type JWTCookeSessionKeys = "jwt_token_for_role"
 export type JWTReferrer = "youtube" | "github" | "twitter" | "notion"
@@ -62,16 +92,13 @@ export type JWTPayload = {
   scope: Partial<RoleAccessPairs>
 }
 
-const createEnumSchema = <T extends string>(values: Array<T>) =>
-  z.enum(values as [T, ...Array<T>])
-
 const referrerSchema = createEnumSchema<JWTReferrer>([])
 const utmMediumSchema = createEnumSchema<UTMMedium>([])
 
 export const jwtPayloadSchema = z.object({
   referrer: referrerSchema,
   utm_medium: utmMediumSchema,
-  scope: roleAccessPairSchema.partial(),
+  scope: roleAccessPairSchema,
 })
 
 export type UserRoleJWTPayload = z.infer<typeof jwtPayloadSchema>
