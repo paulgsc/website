6f576ca05d7181db36840b5ea7ab2d4059e4d547
Paul Gathondu <aulgondu@gmail.com>
2024-08-02 23:09:33 -0700

    feat: add logic to remove searchbar on click

---
+++ self-host/src/components/search-bar/close-searchbar.tsx
@@ -0,0 +1,40 @@
+import type { HTMLAttributes } from "react"
+import { forwardRef } from "react"
+import { parseAsStringLiteral, useQueryState } from "nuqs"
+
+import type { SearchBarRenderType } from "@/types/searchbar"
+import { cn } from "@/lib/utils"
+
+const CloseSearchBarBtn = forwardRef<
+  HTMLButtonElement,
+  HTMLAttributes<HTMLButtonElement>
+>(({ className, ...props }, ref) => {
+  const sortOrder: Array<SearchBarRenderType> = [
+    "fragment",
+    "searchbar",
+  ] as const
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+  const [_, setShowSearch] = useQueryState(
+    "seshow",
+    parseAsStringLiteral(sortOrder)
+      .withDefault("fragment")
+      .withOptions({ clearOnDefault: true })
+  )
+
+  return (
+    <button
+      ref={ref}
+      onClick={() => {
+        setShowSearch("fragment")
+      }}
+      className={cn(
+        "absolute inset-y-0 start-0 z-10 flex items-center ps-3",
+        className
+      )}
+      {...props}
+    />
+  )
+})
+CloseSearchBarBtn.displayName = "CloseSearchBarBtn"
+
+export { CloseSearchBarBtn }
