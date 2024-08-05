9ab9e7ab41ae89ee0994b47c2612e1323094d513
Paul Gathondu <aulgondu@gmail.com>
2024-08-05 02:52:25 -0700

    style: render searchbar on lg media only

--- self-host/src/components/search-bar/index.tsx
+++ self-host/src/components/search-bar/index.tsx
@@ -5,7 +5,7 @@ import { useMediaQuery } from "@/hooks/use-media-query"
 import SearchBarResponsive from "./searchbar-responsive"
 
 const SearchBar = () => {
-  const isDesktop = useMediaQuery("(min-width: 768px)")
+  const isDesktop = useMediaQuery("(min-width: 1024px)")
 
   if (isDesktop) {
     return <SearchBarResponsive />
